dd93174ef0980b26798554be6fa2dcb19f569e1c
Aravindraj Sanmugaraj <aravindraj_sanmugaraj@epam.com>
2025-03-31 13:59:13 +0530

    issue fix

--- app/products/page.js
+++ app/products/page.js
@@ -2,14 +2,14 @@
 
 import { fetchProducts } from "../action"; // Your server-side function to fetch data
 import { ProductsList } from "./ProductList";
-export async function generateStaticParams() {
-  "use cache";
-  const posts = await fetchProducts();
-  console.log(posts);
-  return posts.map((post) => ({
-    id: post.id,
-  }));
-}
+// export async function generateStaticParams() {
+//   "use cache";
+//   const posts = await fetchProducts();
+//   console.log(posts);
+//   return posts.map((post) => ({
+//     id: post.id,
+//   }));
+// }
 export default async function ProductsPage() {
   const products = await fetchProducts(); // Fetch the products server-side
 
